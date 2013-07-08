719b2e7d3b0295cb342cf6efe63e3f7028fabdbb
Triet Le <triet.le@codebox.vn>
2013-07-08 15:58:03 +0700

    test response of methods

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Post do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #Generates a method whose return value is memoized after the first call.
+  let(:post) { Post.new }
+
+  describe "should respond to" do
+    #prefix # for instance method
+    it "#get_title" do
+      post.should respond_to(:get_title)
+    end
+
+    #prefix . for class method
+    it ".find_published_post" do
+      post.class.should respond_to(:find_published_post)
+    end
+  end
 end
