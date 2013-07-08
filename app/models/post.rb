bd2af42803298cf6c315c3eb9ae4161305aff63f
Triet Le <triet.le@codebox.vn>
2013-07-08 15:55:06 +0700

    add methods for sample test

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,16 @@
 class Post < ActiveRecord::Base
-  attr_accessible :description, :title
+  attr_accessible :description, :title, :published
+
+  def get_title
+    if self.published?
+      [self.title, "(P)"].join(' ')
+    else
+      self.title
+    end
+  end
+
+  #find published post, raise RecordNotFound if it doesnt found
+  def self.find_published_post(id)
+    self.find(id, :conditions => { published: true} )
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130708050737) do
+ActiveRecord::Schema.define(:version => 20130708070615) do
 
   create_table "posts", :force => true do |t|
     t.text     "title"
     t.string   "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",                     :null => false
+    t.datetime "updated_at",                     :null => false
+    t.boolean  "published",   :default => false
   end
 
 end
